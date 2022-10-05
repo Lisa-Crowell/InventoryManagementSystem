fadbdef1f1a9a44cfd9b05e40737398a0e4175e6
lisa-crowell <lisa.l.crowell@gmail.com>
2022-10-05 09:55:39 -0500

    add projects and update program

--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using IMS.Data;
+using IMS.Plugins.InMemory;
+using IMS.UseCases.PluginInterfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
 
 var app = builder.Build();
 
