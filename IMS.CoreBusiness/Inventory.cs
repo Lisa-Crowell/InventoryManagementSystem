5a9a4345131c63a48847c8469abdfab5c5159af6
lisa-crowell <lisa.l.crowell@gmail.com>
2022-11-14 19:10:47 -0600

    add attributes

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace IMS.CoreBusiness;
 
 public class Inventory
 {
     public int InventoryId { get; set; }
+    [Required]
     public string InventoryName { get; set; } = string.Empty;
+    [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater than 0")]
     public int Quantity { get; set; }
+    [Range(0, int.MaxValue, ErrorMessage = "Price must be greater than 0")]
     public double Price { get; set; }
 }
